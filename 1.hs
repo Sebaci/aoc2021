ba253570585825d0d83ef0982a6e3e06f44cdc6e
Sebastian Cielemęcki <sc.gman@gmail.com>
2021-12-24 03:36:23 +0100

    day 1 part 1 solution

---
+++ 1.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import System.Environment;
+
+main :: IO ()
+main = do
+    args <- getArgs
+    rawData <- readFile $ head args
+    print $ solve $ map read $ lines rawData
+
+solve :: [Int] -> Int
+solve nums = length $ filter (uncurry (<)) $ zip nums $ tail nums
+
+
