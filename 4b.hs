32bfecf301833afa983a222207ca5fb5d763680b
Sebastian Cielemęcki <sc.gman@gmail.com>
2022-01-28 23:08:37 +0100

    4b solution

---
+++ 4b.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE TupleSections #-}
+
+module Main where
+
+import Data.List
+import System.Environment
+
+data Number = Marked Int | Unmarked Int
+
+type Board = [[Number]]
+
+main :: IO ()
+main = do
+  args <- getArgs
+  input <- readFile $ head args
+
+  let ([rawNumbers] : rawBoards) = splitBy null $ lines input
+  let numbers = map read $ splitBy (== ',') rawNumbers :: [Int]
+  let bingos = map (map (map (Unmarked . read) . words)) rawBoards
+  print $ solve bingos numbers
+
+solve :: [Board] -> [Int] -> Int
+solve boards numbers =
+  lastCalled * score (solvedBingos !! lastBoardIndex)
+  where
+    Just lastBoardIndex = findIndex (not . bingo) $ (snd . last) unsolvedStates
+    (unsolvedStates, (Just lastCalled, solvedBingos) : _) = break (all bingo . snd) bingoHistory
+    bingoHistory = playBingo boards numbers
+
+playBingo :: [Board] -> [Int] -> [(Maybe Int, [Board])]
+playBingo boards = scanl (\(_, b) n -> (Just n, map (markNumberOnBoard n) b)) (Nothing, boards)
+
+markNumberOnBoard :: Int -> Board -> Board
+markNumberOnBoard i = map (map f)
+  where
+    f n@(Unmarked j)
+      | i == j = Marked i
+      | otherwise = n
+    f marked = marked
+
+bingo :: Board -> Bool
+bingo b = any (all isMarked) (b ++ transpose b)
+  where
+    isMarked (Marked _) = True
+    isMarked _ = False
+
+score :: Board -> Int
+score = sum . concatMap (map numberScore)
+  where
+    numberScore (Unmarked n) = n
+    numberScore _ = 0
+
+splitBy :: (a -> Bool) -> [a] -> [[a]]
+splitBy p = unfoldr (\s -> if null s then Nothing else Just (takeWhile (not . p) s, dropWhile p (dropWhile (not . p) s)))
