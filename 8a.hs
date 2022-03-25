d1c35e9f3b632a16ff79fa76d08d7ae6d441da82
Sebastian Cielemęcki <sc.gman@gmail.com>
2022-03-25 23:54:27 +0100

    8a solution

---
+++ 8a.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import Data.List
+import System.Environment
+
+main = do
+  args <- getArgs
+  input <- readFile $ head args
+  print $ solve $ parseInput input
+
+solve :: [([String], [String])] -> Int
+solve = length . filter isUnique . concatMap snd
+  where
+    isUnique number = length number `elem` uniqueLengths
+      where
+        uniqueLengths = [2, 3, 4, 7] -- unique lengths for numbers 1,7,4,8
+
+parseInput :: String -> [([String], [String])]
+parseInput = map parseLine . lines
+  where
+    parseLine line = (words patternsString, words digitsString)
+      where
+        (patternsString, _ : digitsString) = span (/= '|') line
